73369383d162c3aad95dbafc34b29b5cefd07945
lyz04551 <597053768@qq.com>
2025-01-08 09:24:14 +0800

    Update 1_两数之和.py

--- 1_两数之和.py
+++ 1_两数之和.py
@@ -2,12 +2,29 @@ from typing import List
 
 
 # 第一次提交
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         target_dict = {}
+#         for i, item in enumerate(nums):
+#             if target-item in target_dict:
+#                 return target_dict[target-item], i
+#             else:
+#                 target_dict[item] = i
+
+
+# 第二次提交
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        target_dict = {}
-        for i, item in enumerate(nums):
-            if target-item in target_dict:
-                return target_dict[target-item], i
+        
+        nums = sorted((num,i) for i,num in enumerate(nums))
+        
+        left, right = 0, len(nums)-1
+        while left < right:
+            if nums[left][0] + nums[right][0] == target:
+                return nums[left][1], nums[right][1]
+            elif nums[left][0] + nums[right][0] < target:
+                left += 1
             else:
-                target_dict[item] = i
+                right -= 1
 
+print(Solution().twoSum([3,2,4], 6))
